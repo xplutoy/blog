d3c6f48bcd3f4ef9c8c9f64820625c82f3cfa930
yx <yangxue.cs@foxmail.com>
2023-12-11 16:09:58 +0800

    update timestamp

--- scaffold/publish.el
+++ scaffold/publish.el
@@ -23,7 +23,7 @@
       org-export-with-footnotes t
       org-export-with-sub-superscripts nil)
 (setq org-export-global-macros
-      '(("timestamp" . "@@html:<span class=\"timestamp\">[$1]</span>@@")))
+      '(("timestamp" . "@@html:<span class=\"timestamp\">$1 </span>@@")))
 (setq org-html-doctype "html5"
       org-html-html5-fancy t
       org-html-checkbox-type 'html
